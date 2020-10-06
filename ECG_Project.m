72780ecf67cfa36077ed14dc55b063a218a1517d
syamkerai <syamkerai@hotmail.com>
2020-10-06 14:06:47 +1100

    Create ECG_Project.m
    code for final file to call functions
    from tutor

---
+++ ECG_Project.m
@@ -0,0 +1,17 @@
+clear all;
+clc;
+Load Sample_1;
+Threshold=1000;
+Data=Orig_Sig;
+figure(1);
+subplot(2,1,1);
+plot(Data);
+xlim([0 3500]);
+hold on;
+plot([0,length(Data)],[Threshold,Threshold]);
+hold off;
+Data(Data<Threshold)-Threshold;
+subplot(2,1,2);
+plot(Data);
+[n,m]=findpeaks(Data);
+xlim([0 3500]);
