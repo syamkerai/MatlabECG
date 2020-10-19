d5083c48318d0f46289728d469c99cb77a9aad5a
syamkerai <syamkerai@hotmail.com>
2020-10-19 15:48:27 +1100

    limits and chart titles

--- ECG_Project.m
+++ ECG_Project.m
@@ -1,20 +1,28 @@
 clear;
 clc;
-load Sample_1;
+load Sample_3;
 
 threshold=1000;
 rawData=Orig_Sig;
 numSamples = length(rawData);
+maxValue = max(rawData);
+minValue = min(rawData);
+
 
 % render rawData
 figure(1);
 subplot(3,1,1);
-xlim([0 numSamples]); % rawData contains 360*10s samples
 plot(rawData);
+title('raw data')
+xlim([0 numSamples]);
+ylim([minValue maxValue]);
+
 
 % plot threshold line
 hold on
 plot([0, numSamples],[threshold, threshold]);
+xlim([0 numSamples]);
+ylim([minValue maxValue]);
 hold off
 
 % only keep y values above threshold
@@ -22,12 +30,22 @@ aboveThresholdData = rawData;
 aboveThresholdData(aboveThresholdData < threshold) = threshold;
 subplot(3,1,2);
 plot(aboveThresholdData);
+title('thresholded');
+xlim([0 numSamples]);
+ylim([minValue maxValue]);
 
 % count numHeartBeats as number of peaks
-[y,x]=findpeaks(aboveThresholdData);
+[peaksY, peaksX] = findpeaks(aboveThresholdData);
 subplot(3,1,3);
-plot(x,y); 
-numPeaks = size(y,1);
+plot(rawData);
+title('labelled peaks');
+hold on
+scatter(peaksX, peaksY, 'r'); 
+hold off
+xlim([0 numSamples]);
+ylim([minValue maxValue]);
+
+numPeaks = size(peaksX, 1);
 heartRate=(numPeaks * 60) / 10; % 60s in a min, 10s of samples
 disp("Heart Rate is : " + heartRate + " Beats/Min");
 
